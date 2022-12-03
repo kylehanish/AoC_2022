1a5d865b63339a9e9d7e3c3a08787def70ae0af1
Kyle Hanish <kyle.hanish@gmail.com>
2022-12-02 20:17:33 -0600

    day 1 part 2

--- day1/p2/run.rb
+++ day1/p2/run.rb
@@ -1,1 +1,16 @@
 require '../../common/base.rb'
+lines = Input.get_file_lines
+
+current_elve = 0
+mapping = {}
+lines.each_with_index do |value,index|
+    if(value.blank? || index == lines.length - 1)
+        mapping[(mapping.keys.length + 1).to_s] = current_elve 
+        current_elve = 0
+        next
+    end
+    
+    current_elve += value.to_i
+end
+
+puts mapping.values.sort.last(3).sum
